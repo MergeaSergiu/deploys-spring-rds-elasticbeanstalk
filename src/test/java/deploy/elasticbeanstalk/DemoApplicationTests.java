30e20e92e882a0cb9371e25607e529012fb50524
MergeaSergiu <mergeasergiu@gmail.com>
2024-12-21 10:43:53 +0200

    Add Project to Git

---
+++ src/test/java/deploy/elasticbeanstalk/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package deploy.elasticbeanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
